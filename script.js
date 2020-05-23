2108b82f312a299d2bf55aa1eac3d36b3aef4482
ChristopherTaal <63666767+ChristopherTaal@users.noreply.github.com>
2020-05-23 13:02:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// number 1
+var firstVar;
+// number 2
+var name = "Christopher";
+console.log(name);
+//number 3
+var chrisBrother = 3;
+console.log(chrisBrother)
+// number 4
+var division = 20/5;
+console.log(division);
+//number 5
+var Animal = "frog";
+typeof(Animal);
+
+//number 6
+var orderHasShipped = true;
+//number 7
+var orderHasShipped = true;
+if(orderHasShipped === true) {
+    console.log("my orderHasshipped is true");
+}
+else {
+    console.log("my orderHasshipped is false");
+}
+//number 8
+for(var count = 0; count <= 9; count++) {
+    console.log(count);
+}
